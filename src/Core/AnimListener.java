58809e627d8f1be7190387e14df4a2b47626055c
Mennaakram <mennaakram976@gmail.com>
2023-12-01 16:13:36 +0200

    add core

---
+++ src/Core/AnimListener.java
@@ -0,0 +1,10 @@
+package Core;
+
+import javax.media.opengl.GLEventListener;
+import java.awt.event.KeyListener;
+
+public abstract class AnimListener implements GLEventListener, KeyListener {
+ 
+    public String assetsFolderName = "Project/Assets/imgs";
+    
+}
